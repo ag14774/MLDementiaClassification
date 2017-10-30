51ad986702ea07335bc5b548fedfba2c62b63454
Andreas Georgiou <a.georgiou.2014@bristol.ac.uk>
2017-10-30 13:14:29 +0100

    Added ShadeExtraction and Debugger class

---
+++ ml_project/models/utils.py
@@ -0,0 +1,18 @@
+import sys
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class Debugger(BaseEstimator, TransformerMixin):
+    """Debugger"""
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        print("Shape of X: " + str(X.shape))
+        sys.stdout.flush()
+        return X
